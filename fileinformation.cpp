3e7c3cdc40368822a8f6700da8c67a9774e92df2
Valya <valya.ponomarenko.00@mail.ru>
2023-03-20 16:41:30 +0300

    Change class FileInfo
    Add methods GetFileName(),GetFileSize(),IsExist()

--- fileinformation.cpp
+++ fileinformation.cpp
@@ -1,6 +1,20 @@
 #include "fileinformation.h"
 
-FileInformation::FileInformation()
+FileInformation::FileInformation(QString _fileName,int _size, bool _isExist)
 {
-
+    fileName = _fileName;
+    size = _size;
+    isExist = _isExist;
+}
+FileInformation::GetFileName()
+{
+    return fileName;
+}
+FileInformation::GetFileSize()
+{
+    return size;
+}
+FileInformation::IsExist()
+{
+    return isExist;
 }
--- fileinformation.h
+++ fileinformation.h
@@ -1,11 +1,17 @@
 #ifndef FILEINFORMATION_H
 #define FILEINFORMATION_H
-
+#include <QString>
 
 class FileInformation
 {
+    QString fileName;//Имя файла
+    int size;//Размер файла
+    bool isExist;
 public:
-    FileInformation();
+    FileInformation(QString _fileName,int _size, bool _isExist);
+    QString GetFileName();
+    int GetFileSize();
+    bool IsExist();
 };
 
 #endif // FILEINFORMATION_H
