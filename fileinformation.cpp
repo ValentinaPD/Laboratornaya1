bd8790f6b46ffd5301a66c31eb3b262f4a48b4e1
Valya <valya.ponomarenko.00@mail.ru>
2023-03-20 16:10:14 +0300

    Create class FileInformation

---
+++ fileinformation.cpp
@@ -0,0 +1,6 @@
+#include "fileinformation.h"
+
+FileInformation::FileInformation()
+{
+
+}
---
+++ fileinformation.h
@@ -0,0 +1,11 @@
+#ifndef FILEINFORMATION_H
+#define FILEINFORMATION_H
+
+
+class FileInformation
+{
+public:
+    FileInformation();
+};
+
+#endif // FILEINFORMATION_H
