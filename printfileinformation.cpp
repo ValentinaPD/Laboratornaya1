40c3b1172df8a32f67ef488787da0a639f4fc34e
Valya <valya.ponomarenko.00@mail.ru>
2023-03-21 06:50:18 +0300

    Add class PrintFileInformation

---
+++ printfileinformation.cpp
@@ -0,0 +1,6 @@
+#include "printfileinformation.h"
+
+PrintFileInformation::PrintFileInformation()
+{
+
+}
---
+++ printfileinformation.h
@@ -0,0 +1,11 @@
+#ifndef PRINTFILEINFORMATION_H
+#define PRINTFILEINFORMATION_H
+
+
+class PrintFileInformation
+{
+public:
+    PrintFileInformation();
+};
+
+#endif // PRINTFILEINFORMATION_H
